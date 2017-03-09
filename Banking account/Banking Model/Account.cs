ae4c3144a80d711b6e464661d75eaada6a3b4a10
Clase <Clase>
2017-03-08 20:51:28 -0500

    Progra 1

--- Banking account/Banking Model/Account.cs
+++ Banking account/Banking Model/Account.cs
@@ -9,13 +9,34 @@ namespace Banking_Model
     public class Account
     {
         private string _name;
-        public string getName()
+        /*public string getName()
         {
             return _name;
         }
         public void setName(string name)
         {
             _name = name;
+        }*/
+        /*public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+
+        }*/
+        public string Name { get; set; }
+        
+        public Account(){
+        
+        }
+        public Account(string name)
+        {
+            Name = name;
         }
     }
 }
